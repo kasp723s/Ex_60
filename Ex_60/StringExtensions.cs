44be2e2947c5d9801b6e64b29b133eb87a46976c
Kasper Bjørnstrup Andersen <kasp723s@edu.eal.dk>
2019-03-13 09:09:39 +0100

    Add project files.

---
+++ Ex_60/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_60
+{
+    public static class StringExtensions
+    {
+        //public static int Shift
+        //{
+        //    get { return str.Split; }
+
+        //}
+        public static int Shift(this string str)
+        {
+            int Shift = str.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Length;
+            return Shift;
+        }
+    }
+}
